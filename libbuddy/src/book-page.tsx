aa7d178c5707858308e9c7b5bfe7dcc804f8bfb0
JohnnyLe507 <Johnnyle507@yahoo.com>
2025-04-28 04:13:26 -0700

    Adjusted book-page and added author-page
    Added more details to the page regarding the specific book and created an author-page.

--- libbuddy/src/book-page.tsx
+++ libbuddy/src/book-page.tsx
@@ -3,21 +3,42 @@ import './book-page.css'
 import './App.css'
 import axios from 'axios'
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function BookPage() {
     const { id } = useParams();
     const [book, setBook] = useState<any>(null);
+    const [author, setAuthor] = useState<any>(null);
+    const [authorId, setAuthorId] = useState<any>(null);
+    const [showFullDescription, setShowFullDescription] = useState(false);
+
+    const toggleDescription = () => setShowFullDescription(prev => !prev);
+
+    const shortDescription = book?.description?.substring(0, 300)+"..." || "No description available";
 
     useEffect(() => {
         const fetchBookDetails = async () => {
           try {
             const response = await axios.get(`http://localhost:3000/book/${id}`);
-            setBook(response.data);
+            const result = response.data;
+            setBook(result);
+            fetchAuthorDetails(result);
           } catch (error) {
             console.error(error);
-          }
+          } 
         };
-    
+
+        const fetchAuthorDetails = async (data: any) => {
+          try {
+            const authorRaw = data.authors?.[0]?.author?.key || data.authors?.[0]?.key;
+            const authorIdTemp = authorRaw?.split('/').pop();
+            setAuthorId(authorIdTemp);
+            const response = await axios.get(`http://localhost:3000/authors/${authorIdTemp}`);
+            setAuthor(response.data);
+          } catch (error) {
+            console.error(error);
+          };
+        }
         fetchBookDetails();
     }, [id]);
 
@@ -38,17 +59,29 @@ function BookPage() {
             >Sign Up</button>
             </ul>
         </nav>
-            <div className="book-page">
-                <h2>{book.title}</h2>
-                <p>{book.author || "No author available"}</p>
-                {book.covers?.length > 0 && (
-                <img
-                    src={`https://covers.openlibrary.org/b/id/${book.covers[0]}-L.jpg`}
-                    alt={`Cover of ${book.title}`}
-                />
+        <div className="book-page">
+            <h1>{book.title}</h1>
+            {book.covers?.length > 0 && (
+            <img
+                src={`https://covers.openlibrary.org/b/id/${book.covers[0]}-L.jpg`}
+                alt={`Cover of ${book.title}`}
+            />
+            )}
+            <p>{author?.personal_name ? (
+                <Link to={`/author/${authorId}`}>{author.personal_name}</Link>
+            ) : (
+                "No author available"
+            )}
+            </p>
+            <h2>About this book</h2>
+            <p className='description'>{showFullDescription ? book.description : shortDescription}
+                {book.description.length > 300 && (
+                    <button onClick={toggleDescription}>
+                        {showFullDescription ? "See less" : "See more"}
+                    </button>
                 )}
-                <p>{book.description || "No description available"}</p>
-            </div>
+            </p>
+        </div>
         </>
     )
 }
--- libbuddy/src/home.tsx
+++ libbuddy/src/home.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import App from './App';
 import BookPage from './book-page';
+import AuthorPage from './author-page';
 
 function Home() {
     return (
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/book/works/:id" element={<BookPage />} />
+            <Route path="/author/:id" element={<AuthorPage />} />
         </Routes>
     )
 }
